63b428a68fc9c1f6fa86afb89c6f672889d34f81
Jessica Perez <1jessicaperez@gmail.com>
2020-09-08 15:36:40 -0400

    Added authentication info

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const postRoutes = require("./posts");
+const userController = require("../../controllers/userController");
 
 // Post routes
 router.use("/posts", postRoutes);
 
 module.exports = router;
+
+//ADD CODE FOR LOGIN
