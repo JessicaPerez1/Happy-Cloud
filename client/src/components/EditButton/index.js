88f458a278cabab49ba8c7b924f301c176182085
Michael Haber <michaeladam.haber@gmail.com>
2020-09-10 17:51:24 -0400

    Added edit button component

--- client/src/components/EditButton/index.js
+++ client/src/components/EditButton/index.js
@@ -1,2 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+
+function editButton() {
+  function edit(e) {}
+
+  return (
+    <div>
+      <Link to="/editpage">
+        <Button variant="contained" color="primary" onClick={edit}>
+          Edit
+        </Button>
+      </Link>
+    </div>
+  );
+}
+
+export default editButton;
+
+// import React from "react";
+// import { Redirect } from "react-router-dom";
+
+// class Edit extends React.Component {
+//   render() {
+//     return (
+//       <form>
+//         <input placeholder="daily post edit" type="postEdit" />
+//         <button onClick={this.onSubmit}>Save</button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default Login;
 //include an edit button
+
 //add it to where ever we need it
