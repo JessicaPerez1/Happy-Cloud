cf08a7da8674429df7b48bab9b091d8497b252a4
Michael Haber <michaeladam.haber@gmail.com>
2020-09-09 21:23:58 -0400

    Added preliminary api code

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,46 @@
 //user's profile page
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import Prompt from "../components/Prompt";
 import WeekHistory from "../components/WeekHistory";
 import GenerateCloud from "../components/GenerateCloud";
+import API from "../utils/API";
 
-function Profile() {
+class Profile extends Component {
+  state = {
+    posts: [],
+  };
   //const userstate, setUserState, useState =
-  const [userState, setUserState] = useState([]);
+  // const [userState, setUserState] = useState([]);
   //load user's info with useEffect()
-  useEffect(() => {
-    loadUserInfo();
-  }, []);
+  // useEffect(() => {
+  //   loadUserInfo();
+  // }, []);
+
+  componentDidMount() {
+    this.getRecentPosts();
+  }
 
   //Load all user's info from db
-  function loadUserInfo() {}
+  loadUserInfo() {}
 
-  return;
+  getRecentPosts = () => {
+    API.getRecentPosts()
+      .then((res) =>
+        this.setState({
+          posts: res.data,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <WeekHistory />
+      </div>
+    );
+  }
+
   //jumbotron with personalized welcome message "welcome userName" ===> Maybe create seperate component?
   //include Prompt Component
   //include Week history component for user to be able to view
