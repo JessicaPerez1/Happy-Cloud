2e60b5a5cc95b96a9acaea343f6110bf24f9f49a
Jessica Perez <1jessicaperez@gmail.com>
2020-09-10 18:42:37 -0400

    Clear LocalStorage and Get Localstorage added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useGlobalContext } from "./utils/GlobalContext";
 import AuthenticatedApp from "./components/AuthenticatedApp";
 import UnauthenticatedApp from "./components/UnauthenticatedApp";
+import { LOGIN } from "./utils/actions";
+import { useHistory } from "react-router-dom";
 // import Profile from "./pages/Profile";
 // import History from "./pages/History";
 // import Login from "./pages/Login";
@@ -11,17 +13,19 @@ import UnauthenticatedApp from "./components/UnauthenticatedApp";
 
 function App() {
   const [state, dispatch] = useGlobalContext();
-  //check if htere is authentiated user
-  // useEffect(() => {
-  //   //GET INFO FROM LOCALSTORAGE
-  //   // const user = JSON.parse(localStorage.getItem("authuser"));
-  //   // if (user.token) {
-  //   //   dispatch ({
-  //   //     type: LOGIN,
-  //   //     user: user,
-  //   //   })
-  //   // }
-  // }, []);
+  const history = useHistory();
+  //check if there is an authenticated user
+  useEffect(() => {
+    //GET INFO FROM LOCALSTORAGE
+    const user = JSON.parse(localStorage.getItem("data")) || {};
+    if (user.token) {
+      dispatch({
+        type: LOGIN,
+        user: user,
+      });
+      history.push("/profile");
+    }
+  }, []);
 
   return (
     // <Router>
