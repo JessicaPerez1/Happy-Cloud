9dffe4177a0419edc140a6a5796aba9f529d9f90
Marian Gude <mkgude@gmail.com>
2020-09-10 12:03:01 -0500

    added logout functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,37 +3,26 @@ import React, { useState, useEffect } from "react";
 import Prompt from "../components/Prompt";
 import WeekHistory from "../components/WeekHistory";
 import GenerateCloud from "../components/GenerateCloud";
-import { useGlobalContext } from "../context/GlobalContext";
-import { LOGOUT } from "../context/actions";
-import { useHistory } from "react-router-dom";
+import Logout from "../components/Logout";
+import Nav from "../components/Nav";
 
 function Profile() {
-  //const userstate, setUserState, useState =
-  const [userState, setUserState] = useState([]);
+  // const userstate, setUserState, useState =
+  // const [userState, setUserState] = useState([]);
 
-  const [state, dispatch] = useGlobalContext();
-  const history = useHistory();
+  // //load user's info with useEffect()
+  // useEffect(() => {
+  //   loadUserInfo();
+  // }, []);
 
-  const handleLogout = () => {
-    dispatch({
-      type: LOGOUT,
-    });
-    history.push("/");
-    console.log("you've been logged out");
-  };
-
-  //load user's info with useEffect()
-  useEffect(() => {
-    loadUserInfo();
-  }, []);
-
-  //Load all user's info from db
-  function loadUserInfo() {}
+  // //Load all user's info from db
+  // function loadUserInfo() {}
 
   return (
     <div>
+      <Nav />
       <p>Congrats! You're logged in.</p>
-      <button onClick={handleLogout}>Logout</button>
+      <Logout />
     </div>
   );
   //jumbotron with personalized welcome message "welcome userName" ===> Maybe create seperate component?
