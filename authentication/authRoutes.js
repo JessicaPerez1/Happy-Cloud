65bcd223b29cea6afe0c579b048760431e004e54
Marian Gude <mkgude@gmail.com>
2020-09-11 14:21:42 -0500

    updated post controller to create a post

--- authentication/authRoutes.js
+++ authentication/authRoutes.js
@@ -19,8 +19,8 @@ Router.post("/login", function (req, res) {
         res.send(err);
       }
       const token = jwt.sign(user.toJSON(), "superSecret");
-      const { email } = user;
-      return res.json({ email, token });
+      const { email, _id } = user;
+      return res.json({ email, token, _id, message: "you are logged in" });
     });
   })(req, res);
 });
