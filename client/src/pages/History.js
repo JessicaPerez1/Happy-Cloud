ed6e8ca24f0d3066382cb5d0d38eab48b13a1d61
Jessica Perez <1jessicaperez@gmail.com>
2020-09-11 11:56:50 -0400

    Nav completed

--- client/src/pages/History.js
+++ client/src/pages/History.js
@@ -3,6 +3,7 @@ import React from "react";
 import API from "../utils/API";
 import Card from "../components/DailyPost";
 import Post from "../components/Post";
+import Nav from "../components/Nav";
 
 import List from "@material-ui/core/List";
 
@@ -27,27 +28,30 @@ class History extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <h1 className="text-center">
-          <strong>What have you been thankful for??</strong>
-        </h1>
-        <h2 className="text-center">Posting history for your HappyCloud</h2>
-        <Card title="Saved Posts" icon="download">
-          {this.state.posts.length ? (
-            <List>
-              {this.state.posts.map((post) => (
-                <Post
-                  key={post._id}
-                  word={post.word}
-                  date={post.date}
-                  owner={post.owner}
-                />
-              ))}
-            </List>
-          ) : (
-            <h2 className="text-center">No Saved Posts</h2>
-          )}
-        </Card>
+      <div>
+        <Nav />
+        <div className="container">
+          <h1 className="text-center">
+            <strong>What have you been thankful for??</strong>
+          </h1>
+          <h2 className="text-center">Posting history for your HappyCloud</h2>
+          <Card title="Saved Posts" icon="download">
+            {this.state.posts.length ? (
+              <List>
+                {this.state.posts.map((post) => (
+                  <Post
+                    key={post._id}
+                    word={post.word}
+                    date={post.date}
+                    owner={post.owner}
+                  />
+                ))}
+              </List>
+            ) : (
+              <h2 className="text-center">No Saved Posts</h2>
+            )}
+          </Card>
+        </div>
       </div>
     );
   }
