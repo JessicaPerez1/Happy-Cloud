3beade8343bdb9bd2cbdc4e37068c7d7245faee0
Michael Haber <michaeladam.haber@gmail.com>
2020-09-13 19:16:53 -0400

    Added word cloud api

--- client/src/components/GenerateCloud/index.js
+++ client/src/components/GenerateCloud/index.js
@@ -2,12 +2,29 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import { usePostContext } from "../../utils/GlobalState";
-import { LOADING, UPDATE_POSTS } from "../../utils/actions";
+// import { LOADING, UPDATE_POSTS } from "../../utils/actions";
 import { Button } from "@material-ui/core";
 import API from "../../utils/API";
 
 function generateCloud() {
   const [state, dispatch] = usePostContext();
+
+  function runCloud(posts) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("root").innerHTML = this.responseText;
+      }
+    };
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    xhttp.open(
+      "GET",
+      proxyurl + "https://quickchart.io/wordcloud?text=" + posts,
+      true
+    );
+    xhttp.send();
+    console.log("https://quickchart.io/wordcloud?text=" + posts);
+  }
   function handleCloudGenerator() {
     const user = JSON.parse(localStorage.getItem("data"));
     console.log(user[0].id);
@@ -21,6 +38,8 @@ function generateCloud() {
           generatedWords.push(element.post);
         });
         console.log(generatedWords);
+        const finalWords = generatedWords.join(" ");
+        runCloud(finalWords);
       })
       .catch((err) => console.log(err));
   }
