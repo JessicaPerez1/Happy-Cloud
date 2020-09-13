7112f0528155e2091fcb50aaa79ea6ecfd536844
Michael Haber <michaeladam.haber@gmail.com>
2020-09-13 17:16:51 -0400

    Worked on generate cloud component

--- client/src/components/GenerateCloud/index.js
+++ client/src/components/GenerateCloud/index.js
@@ -1,10 +1,13 @@
 // button to click to generate word cloud
 import React from "react";
 // import { Link } from "react-router-dom";
+import { usePostContext } from "../../utils/GlobalState";
+import { LOADING, UPDATE_POSTS } from "../../utils/actions";
 import { Button } from "@material-ui/core";
 import API from "../../utils/API";
 
 function generateCloud() {
+  const [state, dispatch] = usePostContext();
   function handleCloudGenerator() {
     const user = JSON.parse(localStorage.getItem("data"));
     console.log(user[0].id);
@@ -12,12 +15,12 @@ function generateCloud() {
     let generatedWords = [];
     API.getPosts(userId)
       .then((results) => {
-        dispatch({
-          type: UPDATE_POSTS,
-          posts: results.data.post,
+        console.log(results);
+
+        results.data.filter((element) => {
+          generatedWords.push(element.post);
         });
-        console.log(posts);
-        generatedWords;
+        console.log(generatedWords);
       })
       .catch((err) => console.log(err));
   }
