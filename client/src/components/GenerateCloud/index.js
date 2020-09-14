08cef6af07e1cbbaf5bc7650eae3e301268e35f5
Marian Gude <mkgude@gmail.com>
2020-09-14 15:59:59 -0500

    added stying to cloud page

--- client/src/components/GenerateCloud/index.js
+++ client/src/components/GenerateCloud/index.js
@@ -1,19 +1,86 @@
 // button to click to generate word cloud
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import ReactHtmlParser from "react-html-parser";
 import { Button } from "@material-ui/core";
+import API from "../../utils/API";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+  buttonBox: {
+    height: "70px"
+  },
+  button: {
+    background: "#20B2AA"
+
+  },
+  generatedCloud: {
+    margin: "auto",
+    display: "block",
+    height: "400px",
+    width: "400px",
+    border: "2px solid grey",
+    borderRadius: "16px"
+  },
+  cloudDiv: {
+
+  }
+}));
 
 function generateCloud() {
-  const history = useHistory();
+  const classes = useStyles();
+  const [svgFile, setSvgFile] = useState();
 
-  const handleCloudPage = () => {
-    history.push("/cloud");
-  };
+  function runCloud(posts) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        return this.response;
+      }
+      setSvgFile(this.response);
+
+    };
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    xhttp.open(
+      "GET",
+      proxyurl + "https://quickchart.io/wordcloud?text=" + posts,
+      true
+    );
+    xhttp.send();
+    console.log("https://quickchart.io/wordcloud?text=" + posts);
+  }
+
+  function handleCloudGenerator() {
+    const user = JSON.parse(localStorage.getItem("data"));
+    console.log(user[0].id);
+    const userId = user[0].id;
+    let generatedWords = [];
+    API.getPosts(userId)
+      .then((results) => {
+        console.log(results);
+
+        results.data.filter((element) => {
+          generatedWords.push(element.post);
+        });
+        console.log(generatedWords);
+        const finalWords = generatedWords.join(" ");
+        runCloud(finalWords);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
     <div>
-      <Button variant="contained" color="primary" onClick={handleCloudPage}>
-        <Link to="/cloud"> Generate your Cloud Page </Link>
+      <Box className={classes.buttonBox}>
+        <Button
+          variant="contained"
+          onClick={handleCloudGenerator}
+        >
+          Generate Cloud
       </Button>
+      </Box>
+
+      <div className={classes.cloudDiv}><Box className={classes.generatedCloud}> {ReactHtmlParser(svgFile)} </Box></div>
+
 
     </div>
   );
--- client/src/pages/Cloud.js
+++ client/src/pages/Cloud.js
@@ -1,66 +1,28 @@
 // button to click to generate word cloud
-import React, { useState } from "react";
-import ReactHtmlParser from "react-html-parser";
-import { Link } from "react-router-dom";
-// import { LOADING, UPDATE_POSTS } from "../../utils/actions";
-import { Button } from "@material-ui/core";
-import API from "../utils/API";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Nav from "../components/Nav";
+import GenerateCloud from "../components/GenerateCloud"
 
-function generateCloud() {
-  const [svgFile, setSvgFile] = useState();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: "#EAEAF8",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "100%",
+    flexGrow: 1,
+    margin: 0
+  },
+}));
 
-  function runCloud(posts) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        return this.response;
-      }
-      setSvgFile(this.response);
-
-    };
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    xhttp.open(
-      "GET",
-      proxyurl + "https://quickchart.io/wordcloud?text=" + posts,
-      true
-    );
-    xhttp.send();
-    console.log("https://quickchart.io/wordcloud?text=" + posts);
-  }
-
-  function handleCloudGenerator() {
-    const user = JSON.parse(localStorage.getItem("data"));
-    console.log(user[0].id);
-    const userId = user[0].id;
-    let generatedWords = [];
-    API.getPosts(userId)
-      .then((results) => {
-        console.log(results);
-
-        results.data.filter((element) => {
-          generatedWords.push(element.post);
-        });
-        console.log(generatedWords);
-        const finalWords = generatedWords.join(" ");
-        runCloud(finalWords);
-      })
-      .catch((err) => console.log(err));
-  }
+function Cloud() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <Nav />
-      <Link to="/cloud"> Generate your Cloud Page </Link>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleCloudGenerator}
-      >
-        Generate Cloud
-      </Button>
-      <div> {ReactHtmlParser(svgFile)} </div>
+      <GenerateCloud />
     </div>
   );
 }
-
-export default generateCloud;
+export default Cloud;
