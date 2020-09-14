465ce6bed964264dcbe58f205f94d604b1ba1dcf
Marian Gude <mkgude@gmail.com>
2020-09-13 20:28:06 -0500

    created cloud.js page

--- client/src/components/GenerateCloud/index.js
+++ client/src/components/GenerateCloud/index.js
@@ -1,59 +1,23 @@
 // button to click to generate word cloud
 import React from "react";
-// import { Link } from "react-router-dom";
-import { usePostContext } from "../../utils/GlobalState";
-// import { LOADING, UPDATE_POSTS } from "../../utils/actions";
+import { Link, useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
-import API from "../../utils/API";
 
 function generateCloud() {
-  const [state, dispatch] = usePostContext();
+  const history = useHistory()
 
-  function runCloud(posts) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("root").innerHTML = this.responseText;
-      }
-    };
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    xhttp.open(
-      "GET",
-      proxyurl + "https://quickchart.io/wordcloud?text=" + posts,
-      true
-    );
-    xhttp.send();
-    console.log("https://quickchart.io/wordcloud?text=" + posts);
-  }
-  function handleCloudGenerator() {
-    const user = JSON.parse(localStorage.getItem("data"));
-    console.log(user[0].id);
-    const userId = user[0].id;
-    let generatedWords = [];
-    API.getPosts(userId)
-      .then((results) => {
-        console.log(results);
-
-        results.data.filter((element) => {
-          generatedWords.push(element.post);
-        });
-        console.log(generatedWords);
-        const finalWords = generatedWords.join(" ");
-        runCloud(finalWords);
-      })
-      .catch((err) => console.log(err));
+  const handleCloudPage = () => {
+    history.push("/cloud")
   }
   return (
     <div>
-      {/* <Link to="/editpage"> */}
       <Button
         variant="contained"
         color="primary"
-        onClick={handleCloudGenerator}
+        onClick={handleCloudPage}
       >
-        Generate Cloud
+        <Link to="/cloud"> Generate your Cloud Page </Link>
       </Button>
-      {/* </Link> */}
     </div>
   );
 }
