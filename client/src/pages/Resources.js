c5fa1c5a2c491a2be682ccd3f733f0f6913120fb
Jessica Perez <1jessicaperez@gmail.com>
2020-09-18 13:10:49 -0400

    Added resourcecard component

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,12 +1,13 @@
-import React from "react"
-import Nav from "../components/Nav"
+import React from "react";
+import Nav from "../components/Nav";
+import ResourceCard from "../components/ResourceCard";
 
 function Resources() {
-    return (
-        <div>
-            <Nav />
-            <p>This is the page where we will have all resources show up for users that want more information on gratitude journals.</p>
-        </div>
-    )
+  return (
+    <div>
+      <Nav />
+      <ResourceCard />
+    </div>
+  );
 }
-export default Resources
+export default Resources;
